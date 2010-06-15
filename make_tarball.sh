ed372600d926ed5b4e6339434a515ea67646794a
Fred Emmott <fred.emmott@mendeley.com>
2010-06-15 17:57:05 +0100

    s/scribe/zkdns/ in make_tarball.sh

--- make_tarball.sh
+++ make_tarball.sh
@@ -7,6 +7,6 @@ ORIGINAL_REVISION=$(git rev-parse HEAD)
 
 VERSION="${DATE}+$(git rev-parse ${REVISION})"
 
-git archive --format=tar --prefix=scribe-${VERSION}/ ${REVISION} | gzip -9 > zkdns-${VERSION}.tar.gz
+git archive --format=tar --prefix=zkdns-${VERSION}/ ${REVISION} | gzip -9 > zkdns-${VERSION}.tar.gz
 
 echo "Created zkdns-${VERSION}.tar.gz"
